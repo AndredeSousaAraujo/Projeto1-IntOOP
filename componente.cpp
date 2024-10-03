36d1f36e5c8e9e18631e5fa91835ece4682870a0
zamplonio <lucas.zamplonio@gmail.com>
2024-10-03 01:20:24 +0000

    fizemo coisa em

--- componente.cpp
+++ componente.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#pragma once
 using namespace std;
 
 class Componente{
+    protected:
+        int valor;
+
     private:
         bool ligado;
         bool conectado;
         string nome; 
-        int valor;
+        
 
     public:
         Componente(string nome, bool ligado, bool conectado, int valor):
--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include "componente.cpp"
+#include "sensor.cpp"
+#include "temperatura.cpp"
+#include "umidade.cpp"
+#include "luminosidade.cpp"
 using namespace std;
 
 int main(){
-    int ale = rand();
-    cout << ale << endl;
-    Componente vent("Ventilador", false, false, 10);
+    // int ale = rand();
+    //cout << ale << endl;
+    //Componente vent("Ventilador", false, false, 10);
+    Temperatura t("Term1", false, false, 0);
+    Umidade u("Hum1", false, false, 0);
+    Luminosidade l("Lum1", false, false, 10);
+    t.getTemperaturaF();
+    u.getUmidadeRelativa();
+    l.estaClaro();
 
-    cout << "Valor: " << vent.getValor() << endl;
-    vent.ligar();
-    vent.desligar();
-    vent.conectar();
-    vent.desconectar();
+
+
+
+
 
 }
